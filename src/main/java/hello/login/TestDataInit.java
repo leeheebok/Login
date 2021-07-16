42818ca198c1db38c02830aad1e6bca405c962ed
leeheebok <82364286+leeheebok@users.noreply.github.com>
2021-07-16 17:12:25 +0900

    Revert "Filter intercepter"

--- src/main/java/hello/login/TestDataInit.java
+++ src/main/java/hello/login/TestDataInit.java
@@ -2,7 +2,6 @@ package hello.login;
 
 import hello.login.domain.item.Item;
 import hello.login.domain.item.ItemRepository;
-import hello.login.domain.member.Member;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
@@ -10,7 +9,7 @@ import javax.annotation.PostConstruct;
 
 @Component
 @RequiredArgsConstructor
-public class  TestDataInit {
+public class TestDataInit {
 
     private final ItemRepository itemRepository;
 
@@ -21,11 +20,6 @@ public class  TestDataInit {
     public void init() {
         itemRepository.save(new Item("itemA", 10000, 10));
         itemRepository.save(new Item("itemB", 20000, 20));
-
-        Member member = new Member();
-        member.setLoginId("test");
-        member.setPassword("test!");
-
     }
 
 }
